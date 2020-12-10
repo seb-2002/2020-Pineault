7d74c5106452ce88722fb71a140b36adfc801bfb
Sebastian Kann <sebastiankann@gmail.com>
2020-12-10 16:51:39 -0500

    commented out font-face imports

--- js/scroll.js
+++ js/scroll.js
@@ -1,21 +1,19 @@
 //when clicking on .anchorElm, the page scrolls to #scrollToIt
 let anchorElm = "img-top-click";
-let scrollToIt = "anchor-scroll"
+let scrollToIt = "anchor-scroll";
 
 //the speed of the scroll
 let speed = 9000;
 
 //when this function is called, the page scrolls to scrollAnchor
-const scrollToAnchor = function(anId){
-  let aTag = $("#"+anId)
-   $("html,body").animate({ scrollTop: aTag.offset().top }, speed);
-}
-
+const scrollToAnchor = function (anId) {
+  let aTag = $("#" + anId);
+  $("html,body").animate({ scrollTop: aTag.offset().top }, speed);
+};
 
 $(document).ready(function () {
-  $("a."+anchorElm).click(function(event){
+  $("a." + anchorElm).click(function (event) {
     event.preventDefault();
-    scrollToAnchor(scrollToIt);
-  })
+    scrollToAnchor("scrollToIt");
+  });
 });
-
